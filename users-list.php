c271dd7e67c25f63d6761d2450612df8b716f5a6
Ahamed Arshad <achchu.zats@gmail.com>
2023-01-25 07:10:54 +0530

    Initial commit

---
+++ users-list.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Plugin Name:       Users List
+ * Description:       A users list based on API.
+ * Version:           1.0.0
+ * Requires at least: 5.0
+ * Requires PHP:      7.2
+ * Author:            Ahamed Arshad
+ * Author URI:        https://github.com/achchu93/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       aa-userslist
+ * Domain Path:       /languages
+ *
+ * @package AhamedArshad\UsersList
+ */
+
+namespace AhamedArshad\UsersList;
+
+
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+define( 'AA_USERS_LIST_FILE', __FILE__ );
+
+require 'vendor/autoload.php';
